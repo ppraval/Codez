b08b191e6e139efac28c67be5474d84e1e19538a
PravalPattam <ppraval@live.com>
2023-12-20 00:25:31 +0530

    made changes to the code

--- cplusplus/imagesoother.cpp
+++ cplusplus/imagesoother.cpp
@@ -5,66 +5,55 @@ int main()
 {
     vector<vector<int>> img = {{100,200,100}, {200,50,200}, {100,200,100}};
     vector<vector<int>> out_img = img;
-    int sum = 0, n = 0;
-    for(int i = 0; i < img.size(); i++)
+    int a = img.size(), b = img[0].size();
+    for(int i = 0; i < a; i++)
     {
-        for(int j = 0; j < img[0].size(); j++)
+        for(int j = 0; j < b; j++)
         {
+            int sum = 0;
+            int n = 1;
             sum += img[i][j];
-            cout << sum << "\n";
-            n++;
-            if(i + 1 < img.size())
+            if(i + 1 < a)
             {
                 sum += img[i + 1][j];
                 n++;
-                cout << sum << " " << i << " " << j <<  "\n";
             }
-            if(j + 1 < img[0].size())
+            if(j + 1 < b)
             {
                 sum += img[i][j + 1];
                 n++;
-                cout << sum << " " << i << " " << j <<  "\n";
             }
-            if(i + 1 < img.size() && j + 1 < img[0].size())
+            if(i + 1 < a && j + 1 < b)
             {
                 sum += img[i + 1][j + 1];
                 n++;
-                cout << sum << " " << i << " " << j <<  "\n";
             }
-            if(i - 1 > 0)
+            if(i - 1 >= 0)
             {
                 sum += img[i - 1][j];
                 n++;
-                cout << sum << " " << i << " " << j <<  "\n";
             }
-            if(j - 1 > 0)
+            if(j - 1 >= 0)
             {
                 sum += img[i][j - 1];
                 n++;
-                cout << sum << " " << i << " " << j <<  "\n";
             }
-            if(i - 1 > 0 && j - 1 > 0)
+            if(i - 1 >= 0 && j - 1 >= 0)
             {
                 sum += img[i - 1][j - 1];
                 n++;
-                cout << sum << " " << i << " " << j <<  "\n";
             }
-            if(i - 1 > 0 && j + 1 < img[0].size())
+            if(i - 1 >= 0 && j + 1 < b)
             {
                 sum += img[i - 1][j + 1];
                 n++;
-                cout << sum << " " << i << " " << j <<  "\n";
             }
-            if(j - 1 > 0 && i + 1 < img.size())
+            if(j - 1 >= 0 && i + 1 < a)
             {
                 sum += img[i + 1][j - 1];
                 n++;
-                cout << sum << " " << i << " " << j <<  "\n";
             }
             out_img[i][j] = sum / n;
-            cout << "\nFinal average is: " << sum / n << "\n";
-            sum = 0;
-            n = 0;
         }
     }
 
