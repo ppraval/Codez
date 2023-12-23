be8151a1120387d57855bc68a0e13048577ecaaa
PravalPattam <ppraval@live.com>
2023-12-23 22:44:12 +0530

    more codes

--- cplusplus/rmduplicates.cpp
+++ cplusplus/rmduplicates.cpp
@@ -3,13 +3,23 @@ using namespace std;
 
 int main()
 {
-    vector<int> nums = {1, 1, 3};
-    int j = 1;
-    for(int i = 1; i < nums.size(); i++){
-        if(nums[i] != nums[i - 1]){
-            nums[j] = nums[i];
-            j++;
+    vector<int> nums = {1, 1, 3, 3 ,2, 42};
+    int j = 0;
+    int n = nums.size();
+    if(n == 1)
+    {
+        return 1;
+    }
+    for(int i = 0; i < n - 1; i++)
+    {
+        if(nums[i] != nums[i + 1])
+        {
+            nums[j++] = nums[i];
         }
+    }
+    if(nums[n - 1] != nums[n - 2])
+    {
+        nums[j++] = nums[n - 1];
     }
     for(int i = 0; i < j; i++)
     {
