20fa454bf273c32d329c02e31fd6134a4c53691e
PravalPattam <ppraval@live.com>
2023-12-30 17:11:55 +0530

    Made changes to one code

--- cplusplus/balancestrings.cpp
+++ cplusplus/balancestrings.cpp
@@ -1,18 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// int main()
+// {
+//     vector<string> s = {"abc","aabc","bc"};
+//     vector<int> a (26, 0);
+//     for(int i = 0; i < s.size(); i++)
+//     {
+//         for(int j = 0; j < s[i].size(); j++)
+//         {
+//             a[s[i][j] - 'a']++;
+//         }
+//     }
+//     for(int i = 0; i < 26; i++)
+//     {   
+//         if(a[i] % s.size() != 0)
+//         {
+//             cout << "false";
+//             return false;
+//         }
+//     }
+//     cout << "true";
+//     return 0;
+// }
+
+// better code
+
 int main()
 {
-    vector<string> s = {"ab","a"};
+    vector<string> s = {"abc","aabc","bc"};
+    string sum = "";
     vector<int> a (26, 0);
-    for(int i = 0; i < s.size(); i++)
+    int n = s.size();
+    for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < s[i].size(); j++)
-        {
-            a[s[i][j] - 'a']++;
-        }
+        sum += s[i];
     }
-    int new_flag = 0, flag = 0;
+    for(int i = 0; i < sum.size(); i++)
+    {
+        a[sum[i] - 'a']++;
+    }
     for(int i = 0; i < 26; i++)
     {   
         if(a[i] % s.size() != 0)
