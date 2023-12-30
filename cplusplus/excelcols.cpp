8829b569f83e983465e8c86de0039495ab9e2ccd
PravalPattam <ppraval@live.com>
2023-12-31 01:14:23 +0530

    One question that too scammed and i read the question wrong

---
+++ cplusplus/excelcols.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // LESGOOOO I READ THE QUESTION WRONG THIS IS EPIC MORE MARKS ARE GONE AHAHAHAHHAHAHAH
+    // string s = "ZY";
+    // int val = 0;
+    // for(int i = 0; i < s.length(); i++)
+    // {
+    //     val = (val * 26) + (s[i] - 'A' + 1); 
+    // }
+    // cout << val << endl;
+    // return 0;
+    int x = 701;
+    string res = "";
+    while(x > 0)
+    {
+        x--;
+        int val = x % 26;
+        x /= 26;
+        res.push_back(val + 'A');
+    }
+    reverse(res.begin(), res.end());
+    cout << res;
+    return 0;
+}
