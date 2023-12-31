57539b73cf668e225c6f6952ca01fc6acc7b58b0
PravalPattam <ppraval@live.com>
2023-12-31 10:19:47 +0530

    Made changes to that code

--- cplusplus/arraytoaheightbalancedBST.cpp
+++ cplusplus/arraytoaheightbalancedBST.cpp
@@ -1,9 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct TreeNode 
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+
 TreeNode* solve(vector<int>& nums,int low,int high)
 {
-    if(low<=high)
+    if(low <= high)
     {
         int mid = low + (high - low) / 2;
         TreeNode* root = new TreeNode (nums[mid]);
