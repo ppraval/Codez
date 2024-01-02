4bef4e58064a09e7c3177327cf5874c86321cab8
PravalPattam <ppraval@live.com>
2024-01-02 17:09:18 +0530

    incomplete question

--- cplusplus/ismorphicstrings.cpp
+++ cplusplus/ismorphicstrings.cpp
@@ -3,14 +3,28 @@ using namespace std;
 
 int main()
 {
-    string s = "egg";
-    string t = "add";
+    string s = "badc";
+    string t = "baba";
     unordered_map<char, char> map;
     for(int i = 0; i < s.length(); i++)
     {
-        if(map.find(s[i]) == map.end())
-            map[s[i]] = t[i];
-        else    
+        map[s[i]] = t[i];
     }
+    for(auto i : map)
+    {
+        if(map.count(i.second) > 1)
+        {
+            cout << "false";
+        }
+    }
+    // string res = "";
+    // for(int i = 0; i < s.length(); i++)
+    // {
+    //     res.push_back(map[s[i]]);
+    // }
+    // if(res == t)
+    //     cout << "true" << endl;
+    // else
+    //     cout << "false" << endl;
     return 0;
 }
