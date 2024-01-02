b7d8e5b0a6481751760ac5a236630f56c096c87b
PravalPattam <ppraval@live.com>
2024-01-02 22:56:22 +0530

    Another question down the hatch

--- cplusplus/ismorphicstrings.cpp
+++ cplusplus/ismorphicstrings.cpp
@@ -5,26 +5,16 @@ int main()
 {
     string s = "badc";
     string t = "baba";
-    unordered_map<char, char> map;
-    for(int i = 0; i < s.length(); i++)
+    unordered_map<char, char> mp, mp2;
+    for(int i = 0; i < s.length(); i++) 
     {
-        map[s[i]] = t[i];
+        if (mp[s[i]] && mp[s[i]] != t[i]) 
+            cout << false;
+        if (mp2[t[i]] && mp2[t[i]] != s[i]) 
+            cout << false;
+        mp[s[i]] = t[i];
+        mp2[t[i]] = s[i];
     }
-    for(auto i : map)
-    {
-        if(map.count(i.second) > 1)
-        {
-            cout << "false";
-        }
-    }
-    // string res = "";
-    // for(int i = 0; i < s.length(); i++)
-    // {
-    //     res.push_back(map[s[i]]);
-    // }
-    // if(res == t)
-    //     cout << "true" << endl;
-    // else
-    //     cout << "false" << endl;
+    cout << true;
     return 0;
 }
