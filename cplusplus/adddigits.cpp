437eab3070ede7a015cdfa66571100b32409b681
PravalPattam <ppraval@live.com>
2024-01-06 02:45:18 +0530

    another easy one

---
+++ cplusplus/adddigits.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 19;
+    if(num == 0) 
+        return 0;
+    else if(num % 9 == 0) 
+        return 9;
+    else 
+        return num % 9;
+}
