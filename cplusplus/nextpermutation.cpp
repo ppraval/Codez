6500abfe26716997b08c68c28b1651eedd699c13
PravalPattam <ppraval@live.com>
2024-01-23 15:56:26 +0530

    Another medium

---
+++ cplusplus/nextpermutation.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {1, 2, 3};
+    next_permutation(nums.begin(), nums.end());
+    for(int x : nums)
+        cout << x << " "; 
+    return 0;
+}
