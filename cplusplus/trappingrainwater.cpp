df670dd201f8b2948169bf63eb10ad657da584f0
PravalPattam <ppraval@live.com>
2024-02-11 21:21:46 +0530

    a so called hard question

---
+++ cplusplus/trappingrainwater.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
+    int n = height.size();
+    if(n < 3)
+        return 0;
+    int ans = 0;
+    int i = 1, j = n - 2;
+    int left = height[0] , right = height[n - 1];
+    while(i <= j)
+    {
+        if(left < right)
+        {
+            if(height[i] >= left)
+            {
+                left = height[i];
+            }
+            else
+            {
+                ans += left - height[i]; 
+            }
+            i++;
+        }
+        else
+        {
+            if(height[j] >= right)
+            {
+                right = height[j];
+            }
+            else
+            {
+                ans += right - height[j];
+            }
+            j--;
+        }
+    }
+    cout << ans;
+    return 0;
+}
