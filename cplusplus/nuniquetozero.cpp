28de4597cf28a841ac99ea4c220d7e73aed15315
PravalPattam <ppraval@live.com>
2024-03-02 13:11:43 +0530

    two questions that were easy

---
+++ cplusplus/nuniquetozero.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 2;
+    vector<int> nums;
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(i != n - 1)
+        {
+            nums.push_back(i + 1);
+            sum += nums[i];
+        }
+        else
+        {
+            nums.push_back(-1 * sum);
+        }
+    }
+    for(int i : nums)
+        cout << i << " ";
+    return 0;
+}
--- cplusplus/squaresorted.cpp
+++ cplusplus/squaresorted.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+    // Scammed the question based on the O(nlogn) instead of O(n)
     vector<int> nums = {-4,-1,0,3,10};
     for(int i = 0; i < nums.size(); i++)
     {
