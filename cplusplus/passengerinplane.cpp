f56a130e039c18b0484711038c6f267bdc078a1e
PravalPattam <ppraval@live.com>
2024-03-08 02:42:04 +0530

    what is the question that i solved sorry copied

---
+++ cplusplus/passengerinplane.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 2;
+    if(n == 1)
+    {
+        cout << 1.00 << endl;
+    }
+    else
+    {
+        cout << 0.50 << endl;
+    }
+    return 0;
+}
