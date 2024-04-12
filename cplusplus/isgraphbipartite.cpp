45a2cc15f825f8059941a90bf646686b71c0d226
PravalPattam <ppraval@live.com>
2024-04-12 09:15:10 +0530

    make this work

--- cplusplus/isgraphbipartite.cpp
+++ cplusplus/isgraphbipartite.cpp
@@ -1,31 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void bfs(vector<vector<int>> adj_list, int start, vector<bool> visited)
+bool dfs(int vertices, int v, vector<bool>& visited, vector<int>& colour, vector<vector<int>> graph)
 {
-    queue<int> q;
-    visited[start] = true;
-    q.push(start);
-    while(!q.empty())
+    visited[v] = true;
+    for(int u : graph[v])
     {
-        int current = q.front();
-        q.pop();
-        cout << current << " ";
-        for(int i : adj_list[current])
+        if(visited[u] == false)
         {
-            if(visited[i] == false)
+            colour[u] != colour[v];
+            if(!dfs(vertices, u, visited, colour, graph))
             {
-                visited[i] = true;
-                q.push(i);
+                return false;
             }
         }
+        else if(colour[u] == colour[v])
+        {
+            return false;
+        }
     }
+    return true;
 }
 
 int main()
 {
-    vector<vector<int>> graph = {{1,2,3},{0,2},{0,1,3},{0,2}};
+    // vector<vector<int>> graph = {{1,2,3},{0,2},{0,1,3},{0,2}};
+    vector<vector<int>> graph = {{1,3},{0,2},{1,3},{0,2}};
     vector<bool> visited(graph.size(), false);
-    bfs(graph, 0, visited);
+    vector<int> colour(graph.size());
+    colour[0] = 1;
+    if(dfs(graph.size(), 0, visited, colour, graph))
+    {
+        cout << "True";
+        return 1;
+    }
+    cout << "False";
     return 0;
 }
