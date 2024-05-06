fb729c9ab28b223bbab64ad357f55a1095d38de0
PravalPattam <ppraval@live.com>
2024-05-06 16:31:40 +0530

    another question

---
+++ cplusplus/3sum.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {-1,0,1,2,-1,-4};
+    vector<vector<int>> ans;
+    set<vector<int>> s;
+    sort(nums.begin(), nums.end());
+    for(int i = 0; i < nums.size(); i++)
+    {
+        int l = i + 1;
+        int r = nums.size() - 1;
+        while(l < r)
+        {
+            int sum = nums[i] + nums[l] + nums[r];  
+            if(sum == 0)
+            {
+                s.insert({nums[i], nums[l], nums[r]});
+                l++;
+                r--;
+            }
+            else if(sum < 0)
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+    }
+    for(auto &v : s)
+        ans.push_back(v);
+    for(int i = 0; i < ans.size(); i++)
+    {
+        for(int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
