32d6aa53d22a7caa14b5ddce094f298ab62c8e63
PravalPattam <ppraval@live.com>
2024-05-21 19:33:38 +0530

    pain

--- cplusplus/3sum.cpp
+++ cplusplus/3sum.cpp
@@ -6,6 +6,7 @@ int main()
     vector<int> nums = {-1,0,1,2,-1,-4};
     vector<vector<int>> ans;
     set<vector<int>> s;
+    
     sort(nums.begin(), nums.end());
     for(int i = 0; i < nums.size(); i++)
     {
