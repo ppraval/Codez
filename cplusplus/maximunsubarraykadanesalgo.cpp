983c3af205b35fd8dbb83b6cab55d689f3583d0e
PravalPattam <ppraval@live.com>
2024-06-10 22:27:51 +0530

    idk what this is or why this is

---
+++ cplusplus/maximunsubarraykadanesalgo.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+    int max_sum = INT_MIN;
+    int curr_sum = 0;
+    for(int i = 0; i < nums.size(); i++)
+    {
+        curr_sum += nums[i];
+        max_sum = max(max_sum, curr_sum);
+        if(curr_sum < 0)
+        {
+            curr_sum = 0;
+        }
+    }
+    // cout << max_sum;
+    return max_sum;
+}
