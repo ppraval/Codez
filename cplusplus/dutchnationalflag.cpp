cd940fb1c6f273ee1bd6637ae75a394ce8c852d8
PravalPattam <ppraval@live.com>
2024-06-10 23:07:42 +0530

    pain i am in

---
+++ cplusplus/dutchnationalflag.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {2,0,2,1,1,0};
+    int low = 0, mid = 0, high = arr.size() - 1;
+    while(mid <= high)
+    {
+        if(arr[mid] == 0)
+        {
+            swap(arr[mid], arr[low]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid] == 1)
+        {
+            mid++;
+        }
+        else 
+        {
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+    for(int i : arr)
+        cout << i << " ";
+    return 0;
+}
