0460636355e3fe39bfdb223b8eeee45df6ef7e2f
PravalPattam <ppraval@live.com>
2024-06-29 12:30:28 +0530

    more code that i amlearning

---
+++ cplusplus/lengthofthelongestsubarraywithsum0.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {15,-2,2,-8,1,7,10,23};
+    int max_i = 0;
+    int sum = 0;
+    unordered_map<int, int> mp;
+    for(int i = 0; i < nums.size(); i++)
+    {
+        sum += nums[i];
+        if(sum == 0)
+        {
+            max_i = i + 1;
+        }
+        else
+        {
+            if(mp.find(sum) != mp.end())
+            {
+                max_i = max(max_i, i - mp[sum]);
+            }
+            else
+            {
+                mp[sum] = i;
+            }
+        }
+        cout << "sum = " << sum << endl;
+        cout << "max_i = " << max_i << endl;
+        cout << "i = " << i << endl << endl;
+    }
+    cout << max_i;
+    return max_i;
+}
---
+++ cplusplus/longestconsecutivesequence.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {100, 4, 200, 1, 3, 2};
+    unordered_set<int> s;
+    int max_len = INT_MIN;
+    for(int i : nums)
+        s.insert(i);
+    for(int i : nums)
+    {
+        if(s.count(i - 1) > 0)
+        {
+            int j = 0;
+            int len = 1;
+            while(s.count(i + j) > 0)
+            {
+                j++;
+                len++;
+            }
+            max_len = max(len, max_len);
+        }
+    } 
+    cout << max_len;
+    return 0;
+}
