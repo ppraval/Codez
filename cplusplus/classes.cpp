a05f3e0545fb3940cf6d030290c7bb51b34b0d1d
PravalPattam <ppraval@live.com>
2024-07-14 23:17:00 +0530

    fml

--- cplusplus/classes.cpp
+++ cplusplus/classes.cpp
@@ -5,14 +5,20 @@ class pain
 {
     public:
         string s;
-        pain() : s("I LOVE THIS GUN") {cout << "I hate this life" << endl;}
-        pain(string t) : s(t) {cout << "I hate this life x2" << endl;}
+        pain() : s("I LOVE THIS GUN\n") {cout << "pain" << endl;}
+        pain(string t) : s(t) {cout << "pain x2" << endl;}
+
+        void morePain(int p)
+        {
+            cout << "I have a " << p << " long rod shoved up my" << endl;
+        }
 };
 
 int main()
 {
-    pain pain1 = new pain();
+    pain pain1;
     // pain1.s = "I am in so much pain";
     cout << pain1.s;
+    pain1.morePain(10);
     return 0;
 }
