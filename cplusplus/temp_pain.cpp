f55345bab138eb91b55816112927f4ae02c76138
PravalPattam <ppraval@live.com>
2024-08-02 00:26:46 +0530

    i wanna kms

---
+++ cplusplus/temp_pain.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int val;
+    struct Node* next;
+};
+
+typedef struct Node* node;
+
+int main()
+{
+    node root = NULL;
+    return 0;
+}
