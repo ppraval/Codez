edc5ee5f24d3bdc942c8cda7d7f88839de8bec2c
PravalPattam <ppraval@live.com>
2024-08-23 22:35:11 +0530

    she still real pretty tho

--- cplusplus/boxstars.cpp
+++ cplusplus/boxstars.cpp
@@ -4,27 +4,38 @@ using namespace std;
 int main()
 {
     int n = 5;
+    vector<string> ans(n);
     for(int i = 0; i < n; i++)
     {
+        string temp = "";
         for(int j = 0; j < n; j++)
         {
             if(i == 0 || i == n - 1)
             {
-                cout << "*";
+                // cout << "*";
+                temp += "*";
             }
             else
             {
                 if(j == 0 || j == n - 1)
                 {
-                    cout << "*";
+                    // cout << "*";
+                    temp += "*";
                 }
                 else
                 {
-                    cout << " ";
+                    // cout << " ";
+                    temp += " ";
                 }
             }
         }
-        cout << endl;
+        ans[i] = temp;
+        // ans.push_back(temp);
+        // cout << endl;
+    }
+    for(auto x : ans)
+    {
+        cout << x << endl;
     }
     return 0;
 }
