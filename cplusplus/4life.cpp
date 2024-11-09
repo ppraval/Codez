48332055a60b7e194ef0e5f79bb8d0a13044367b
PravalPattam <ppraval@live.com>
2024-11-10 00:23:16 +0530

    4life

---
+++ cplusplus/4life.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pain()
+{
+    cout << "pain" << endl;
+}
+
+int main()
+{
+    int a = 4;
+    cout << "I love her " << a << "life." << endl;
+    cout <<"starting stage of mg";
+    pain();
+    return 0;
+}
