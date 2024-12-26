4cc36c1e5b8453adb2e44cde84a636eab6e06325
PravalPattam <ppraval@live.com>
2024-12-26 20:08:16 +0530

    Re wrote codes

---
+++ cplusplus/cin.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string temp;
+    int t = 5;
+    vector<string> v(5);
+    // int i = 0;
+    // while(t--)
+    // {
+    //     cin >> temp;
+    //     cout << t << endl;
+    //     v[i++] = temp;
+    // }
+    cin >> v[0] >> v[1] >> v[2] >> v[3] >> v[4];
+    int a, b;
+    for(auto x : v)
+        cout << x << endl;
+    return 0;
+}
--- cplusplus/reversewords3.cpp
+++ cplusplus/reversewords3.cpp
@@ -7,12 +7,19 @@ int main()
     string ans = "";
     stringstream ss(s);
     string temp;
+    cout << "the string is: "<< s << endl;
+    // int abhi;
+    // cin >> abhi;
+    int i = 0;
     while(ss >> temp)
     {
+        cout << i++ << endl;
+        cout << temp << endl;
         reverse(temp.begin(), temp.end());
         ans += temp + " ";
     }
     ans.pop_back();
+    cout << "Answer: " << endl;
     cout << ans << endl;
     return 0;
 }
